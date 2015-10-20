a2d16980944e94c21363b48c385879f42e63c078
matheus <contato@matheusgimenez.com.br>
2015-10-20 16:54:58 -0200

    add footer

--- footer.php
+++ footer.php
@@ -9,6 +9,31 @@
  * @since 2.2.0
  */
 ?>
+<footer id="footer-xango" class="col-md-12">
+	<div class="container">
+		<div class="col-md-4 footer-logo">
+			<?php if ( $image = kirki_get_option( 'footer_image' ) ) : ?>
+				<img src="<?php echo esc_url( $image );?>" alt="<?php get_bloginfo( 'name' );?>">
+			<?php else : ?>
+				<?php $image = get_header_image();?>
+				<img src="<?php echo esc_url( $image );?>" alt="<?php get_bloginfo( 'name' );?>">
+			<?php endif;?>
+		</div><!-- .col-md-4 footer-logo -->
+		<div class="col-md-6 footer-text">
+			<?php if ( $text = kirki_get_option( 'footer_text' ) ) : ?>
+				<?php echo apply_filters( 'the_content', $text );?>
+			<?php endif; ?>
+		</div><!-- .col-md-6 footer-text -->
+		<div class="col-md-2 pull-right footer-links">
+			<a href="http://br.wordpress.org">
+				<img src="<?php echo get_template_directory_uri();?>/assets/images/wp-org.png" alt="<?php _e( 'Made with love on WordPress.org', 'odin');?>" title="<?php _e( 'Made with love using WordPress.org', 'odin');?>" />
+			</a>
+			<a href="http://br.wordpress.org">
+				<img src="<?php echo get_template_directory_uri();?>/assets/images/brasa-footer.png" alt="<?php _e( 'By Brasa.art.br', 'odin');?>" title="<?php _e( 'By Brasa.art.br', 'odin');?>" />
+			</a>
+		</div><!-- .col-md-2 pull-right footer-links -->
+	</div><!-- .container -->
+</footer><!-- #footer-xango.col-md-12 -->
 	<?php wp_footer(); ?>
 </body>
 </html>
--- inc/customizer.php
+++ inc/customizer.php
@@ -15,3 +15,41 @@ function xango_kirki_config() {
 	return $args;
 }
 add_filter( 'kirki/config', 'xango_kirki_config' );
+
+/**
+ * Add footer fields
+ */
+/**
+ * Create the setting
+ */
+function brasa_xango_register_section( $wp_customize ) {
+    /**
+     * Add sections
+     */
+    $wp_customize->add_section( 'xango_footer', array(
+        'title'       => __( 'Footer', 'odin' ),
+        'priority'    => 10,
+    ) );
+
+}
+add_action( 'customize_register', 'brasa_xango_register_section' );
+function brasa_xango_kirki_fields( $fields ) {
+    $fields[] = array(
+        'type'     => 'image',
+        'setting'  => 'footer_image',
+        'label'    => __( 'Footer image', 'odin' ),
+        'section'  => 'xango_footer',
+        'default'  => '',
+        'priority' => 1,
+    );
+    $fields[] = array(
+        'type'     => 'textarea',
+        'setting'  => 'footer_text',
+        'label'    => __( 'Footer text', 'odin' ),
+        'section'  => 'xango_footer',
+        'default'  => '',
+        'priority' => 2,
+    );
+    return $fields;
+}
+add_filter( 'kirki/fields', 'brasa_xango_kirki_fields' );
